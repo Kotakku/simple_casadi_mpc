d0686c9902e6eda5898985df167dbf7fe2787a1a
Kotakku <kotakkucu@gmail.com>
2026-01-10 23:45:58 +0900

    add casadi eigen utils
    Signed-off-by: Kotakku <kotakkucu@gmail.com>

--- include/simple_casadi_mpc/casadi_utils.hpp
+++ include/simple_casadi_mpc/casadi_utils.hpp
@@ -7,16 +7,42 @@
 
 namespace casadi_utils {
 
-inline Eigen::MatrixXd to_eigen(const casadi::DM &dm) {
-  Eigen::MatrixXd mat(dm.size1(), dm.size2());
-  std::memcpy(mat.data(), dm.ptr(), sizeof(double) * dm.size1() * dm.size2());
-  return mat;
+static Eigen::MatrixXd to_eigen(const casadi::DM &cas_mat) {
+  Eigen::MatrixXd eig_mat(cas_mat.size1(), cas_mat.size2());
+  for (casadi_int i = 0; i < cas_mat.size1(); ++i) {
+    for (casadi_int j = 0; j < cas_mat.size2(); ++j) {
+      eig_mat(i, j) = static_cast<Scalar>(cas_mat(i, j));
+    }
+  }
+  return eig_mat;
 }
 
-inline casadi::DM to_casadi(const Eigen::MatrixXd &mat) {
-  casadi::DM dm = casadi::DM::zeros(mat.rows(), mat.cols());
-  std::memcpy(dm.ptr(), mat.data(), sizeof(double) * mat.rows() * mat.cols());
-  return dm;
+static casadi::DM to_casadi(const Eigen::MatrixXd &eig_mat) {
+  casadi::DM cas_mat = casadi::DM::zeros(eig_mat.rows(), eig_mat.cols());
+  for (int i = 0; i < eig_mat.rows(); ++i) {
+    for (int j = 0; j < eig_mat.cols(); ++j) {
+      cas_mat(i, j) = eig_mat(i, j);
+    }
+  }
+  return cas_mat;
+}
+
+template <class Sym = casadi::SX>
+static Eigen::Matrix<Sym, Eigen::Dynamic, 1> pack_vector(const Sym &sym) {
+  Eigen::Matrix<Sym, Eigen::Dynamic, 1> vec(sym.size1());
+  for (int i = 0; i < sym.size1(); ++i) {
+    vec(i) = sym(i);
+  }
+  return vec;
+}
+
+template <class Sym = casadi::SX>
+static Sym unpack_vector(const Eigen::Matrix<Sym, Eigen::Dynamic, 1> &vec) {
+  Sym sym = Sym::zeros(vec.size());
+  for (int i = 0; i < vec.size(); ++i) {
+    sym(i) = vec(i);
+  }
+  return sym;
 }
 
 } // namespace casadi_utils
