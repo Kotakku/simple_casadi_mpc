f886ebc8de2f79820e71aa63d4b839f807d3d750
Kotakku <Kotakkucu@gmail.com>
2026-05-09 12:06:50 +0000

    Deploying to gh-pages from @ Kotakku/simple_casadi_mpc@3882d6457ef5ac896c9d2b767e2089f6d8693a84 🚀

--- search/all_0.js
+++ search/all_0.js
@@ -3,5 +3,7 @@ var searchData=
   ['a_20problem_0',['Defining a problem',['../index.html#autotoc_md6',1,'']]],
   ['a_20solver_20backend_1',['Choosing a solver backend',['../index.html#autotoc_md17',1,'']]],
   ['add_5fconstraint_2',['add_constraint',['../classsimple__casadi__mpc_1_1Problem.html#a48bd6a0c0fb320ccce5feec458fd25c5',1,'simple_casadi_mpc::Problem']]],
-  ['add_5fsoft_5fconstraint_3',['add_soft_constraint',['../classsimple__casadi__mpc_1_1Problem.html#a2858872818fbefe936d96c6b2a6a1c96',1,'simple_casadi_mpc::Problem']]]
+  ['add_5fconstraint_5fat_3',['add_constraint_at',['../classsimple__casadi__mpc_1_1Problem.html#a1b2ca0ed9f010a6818f5f218235e0da4',1,'simple_casadi_mpc::Problem']]],
+  ['add_5fsoft_5fconstraint_4',['add_soft_constraint',['../classsimple__casadi__mpc_1_1Problem.html#a2858872818fbefe936d96c6b2a6a1c96',1,'simple_casadi_mpc::Problem']]],
+  ['add_5fsoft_5fconstraint_5fat_5',['add_soft_constraint_at',['../classsimple__casadi__mpc_1_1Problem.html#a4221d17a3fdd0845c6e6daa929af7ad2',1,'simple_casadi_mpc::Problem']]]
 ];
--- search/functions_0.js
+++ search/functions_0.js
@@ -1,5 +1,7 @@
 var searchData=
 [
   ['add_5fconstraint_0',['add_constraint',['../classsimple__casadi__mpc_1_1Problem.html#a48bd6a0c0fb320ccce5feec458fd25c5',1,'simple_casadi_mpc::Problem']]],
-  ['add_5fsoft_5fconstraint_1',['add_soft_constraint',['../classsimple__casadi__mpc_1_1Problem.html#a2858872818fbefe936d96c6b2a6a1c96',1,'simple_casadi_mpc::Problem']]]
+  ['add_5fconstraint_5fat_1',['add_constraint_at',['../classsimple__casadi__mpc_1_1Problem.html#a1b2ca0ed9f010a6818f5f218235e0da4',1,'simple_casadi_mpc::Problem']]],
+  ['add_5fsoft_5fconstraint_2',['add_soft_constraint',['../classsimple__casadi__mpc_1_1Problem.html#a2858872818fbefe936d96c6b2a6a1c96',1,'simple_casadi_mpc::Problem']]],
+  ['add_5fsoft_5fconstraint_5fat_3',['add_soft_constraint_at',['../classsimple__casadi__mpc_1_1Problem.html#a4221d17a3fdd0845c6e6daa929af7ad2',1,'simple_casadi_mpc::Problem']]]
 ];
